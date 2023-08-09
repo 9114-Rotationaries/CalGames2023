b38cfa79721af5d2738d1c3d55b577005c700af1
Rotationaries <rotationaries@gmail.com>
2023-08-08 18:42:00 -0700

    updated

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,64 +4,59 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Translation2d;
+
 public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
   }
 
   public static class DriveConstants {
-    public static final double kMaxSpeed = 10.0; // 3 meters per second
-    public static final double kMaxAngularSpeed = 6; // 1/2 rotation per second
+
+    public static final Translation2d[] moduleTranslations = {new Translation2d(0.381, 0.381), new Translation2d(0.381, -0.381),
+    new Translation2d(-0.381, 0.381), new Translation2d(-0.381, -0.381)};
+    
+    public static final int pigeon = 9;
+    public static final double kMaxSpeed = 4; // 1 meters per second
+    public static final double kMaxAngularSpeed = 12; // 12 radians? per second
 
     //FL-Front Left, DM-Driving Motor, TM-Turning Motor, TE-Turning Encoder
-    public static final int FLDMChannel = 20;
-    public static final int FLTMChannel = 21;
-    public static final int FLDEChannelA = 22;
-    public static final int FLDEChannelB = 23;
-    public static final int FLTEChannelA = 24;
-    public static final int FLTEChannelB = 25;
-    //public static final double FLTEOffsetDegrees = 0;
+    public static final int FLDMChannel = 12;
+    public static final int FLTMChannel = 11;
+    public static final int FLTEChannel = 13;
+    public static final double FLTEOffsetDegrees = 278.174;
 
     public static final int FRDMChannel = 3;
     public static final int FRTMChannel = 2;
-    public static final int FRDEChannelA = 5;
-    public static final int FRDEChannelB = 4;
-    public static final int FRTEChannelA = 7;
-    public static final int FRTEChannelB = 6;
-    //public static final double FRTEOffsetDegrees = 0;
+    public static final int FRTEChannel = 4;
+    public static final double FRTEOffsetDegrees = 343.916;
 
-    public static final int BLDMChannel = 14;
-    public static final int BLTMChannel = 15;
-    public static final int BLDEChannelA = 16;
-    public static final int BLDEChannelB = 17;
-    public static final int BLTEChannelA = 18;
-    public static final int BLTEChannelB = 19;
-    //public static final double BLTEOffsetDegrees = 0;
+    public static final int BLDMChannel = 9;
+    public static final int BLTMChannel = 8;
+    public static final int BLTEChannel = 10;
+    public static final double BLTEOffsetDegrees = 119.707;
 
-    public static final int BRDMChannel = 10;
-    public static final int BRTMChannel = 8;
-    public static final int BRDEChannelA = 9;
-    public static final int BRDEChannelB = 11;
-    public static final int BRTEChannelA = 13;
-    public static final int BRTEChannelB = 12;
-    //public static final double BRTEOffsetDegrees = 0;
+    public static final int BRDMChannel = 6;
+    public static final int BRTMChannel = 5;
+    public static final int BRTEChannel = 7;
+    public static final double BRTEOffsetDegrees = 181.494;
   }
   
-  public static class ModuleConstants {
+  public static class SwerveConstants {
     public static final double kWheelRadius = 0.0508;
-    public static final int kEncoderResolution = 4096;
+    public static final int kEncoderResolution = 42;
 
     public static final double kModuleMaxAngularVelocity = DriveConstants.kMaxAngularSpeed;
-    public static final double kModuleMaxAngularAcceleration = 10; // radians per second squared
+    public static final double kModuleMaxAngularAcceleration = 5; // radians per second squared
 
     // drive PID constants
-    public static final double PIDp = 0.1;
+    public static final double PIDp = 0.2;
     public static final double PIDi = 0;
-    public static final double PIDd = 0;
+    public static final double PIDd = 0.0;
 
     // turning PID constants 
-    public static final double ProfiledPIDp = 0.02;
-    public static final double ProfiledPIDi = 0;
+    public static final double ProfiledPIDp = 0.4;
+    public static final double ProfiledPIDi = 0.0000000;
     public static final double ProfiledPIDd = 0.0001;
 
     public static final double DriveKs = 0;
@@ -70,4 +65,36 @@ public final class Constants {
     public static final double TurnKs = 0;
     public static final double TurnKv = 0;
   }
+
+  public static class VisionConstants{
+    public static final double targetHeight = 0;
+    public static final double cameraHeight = 0;
+    public static final double cameraAngle = 0;
+
+    //MoveToTag Angle PID Constants
+    public static final double VAnglePIDp = 0.1;
+    public static final double VAnglePIDi = 0;
+    public static final double VAnglePIDd = 0;
+
+    //MoveToTag Drive PID Constants
+    public static final double VDrivePIDp = 0.1;
+    public static final double VDrivePIDi = 0;
+    public static final double VDrivePIDd = 0;
+
+    //MoveToTag Rot PID Constants
+    public static final double VRotPIDp = 0.1;
+    public static final double VRotPIDi = 0;
+    public static final double VRotPIDd = 0;
+
+    //Tag Setpoints
+    public static final double alignSetpoint = 0; //Center
+    public static final double driveSetpoint = 0; //Change to how far away from tag desired
+    public static final double rotSetpoint = 0; //Oriented
+
+    //Tolerances
+    public static final double angleTolerance = 0.02;
+    public static final double distanceTolerance = 0.02;
+    public static final double rotTolerance = 0.02;
+  }
+
 }
--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -7,6 +7,8 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 //import com.revrobotics.CANSparkMaxLowLevel;
 import com.revrobotics.REVPhysicsSim;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxRelativeEncoder;
 //import com.revrobotics.CANSparkMaxLowLevel.PeriodicFrame;
 //import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
 //import com.ctre.phoenix.motorcontrol.TalonFXSimCollection;
@@ -15,12 +17,16 @@ import com.revrobotics.REVPhysicsSim;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.XboxController;
 //import edu.wpi.first.wpilibj.drive.RobotDriveBase.MotorType;
@@ -30,76 +36,180 @@ import edu.wpi.first.wpilibj.simulation.EncoderSim;
 import edu.wpi.first.wpilibj.simulation.FlywheelSim;
 import edu.wpi.first.wpilibj.simulation.RoboRioSim;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-//import frc.robot.Constants.DriveConstants;
-import frc.robot.Constants.ModuleConstants;
+import com.ctre.phoenix.sensors.CANCoder;
+import frc.robot.Constants.SwerveConstants;
 import frc.robot.commands.JoystickDrive;
 
 public class SwerveModule extends SubsystemBase {
   
-  private final CANSparkMax driveMotor;
-  private final CANSparkMax turnMotor;
+  private final CANSparkMax m_driveMotor;
+  private final CANSparkMax m_turningMotor;
 
   private FlywheelSim driveSim;
   private FlywheelSim turnSim;
 
-  private final Encoder driveEncoder;
-  private final Encoder turnEncoder;
+  private final RelativeEncoder m_driveEncoder;
+  private final RelativeEncoder m_turningEncoder;
+  private final CANCoder m_angleEncoder;
 
-  private final PIDController drivePIDController = new PIDController(ModuleConstants.PIDp, ModuleConstants.PIDi, ModuleConstants.PIDd);
-  private final PIDController turnPIDController = new PIDController(ModuleConstants.ProfiledPIDp, ModuleConstants.ProfiledPIDi, ModuleConstants.ProfiledPIDd);
+  private final PIDController m_drivePIDController = new PIDController(SwerveConstants.PIDp, SwerveConstants.PIDi, SwerveConstants.PIDd);
 
-  private final SimpleMotorFeedforward driveFeedforward = new SimpleMotorFeedforward(ModuleConstants.DriveKs, ModuleConstants.DriveKv);
-  private final SimpleMotorFeedforward turnFeedforward = new SimpleMotorFeedforward(ModuleConstants.TurnKs, ModuleConstants.TurnKv);
+  // Gains are for example purposes only - must be determined for your own robot!
+  private final ProfiledPIDController m_turningPIDController =
+      new ProfiledPIDController(
+          SwerveConstants.ProfiledPIDp,
+          SwerveConstants.ProfiledPIDi,
+          SwerveConstants.ProfiledPIDd,
+          new TrapezoidProfile.Constraints(
+              SwerveConstants.kModuleMaxAngularVelocity, SwerveConstants.kModuleMaxAngularAcceleration));
+
+  //private Encoder fakeDriveEncoder = new Encoder(1, 2, 0, true);
+  //private Encoder fakeTurnEncoder = new Encoder(3, 4, 5, true);
 
   //private final JoystickDrive drive = new JoystickDrive(m_controller, null, false)
 
-  private EncoderSim driveEncoderSim;
-  private EncoderSim turnEncoderSim;
+  //private EncoderSim driveEncoderSim = new EncoderSim(fakeDriveEncoder);
+  //private EncoderSim turnEncoderSim = new EncoderSim(fakeTurnEncoder);
 
   public double turnOutput;
   public double driveOutput;
 
+  private double m_simDriveEncoderPosition;
+  private double m_simDriveEncoderVelocity;
+  private double m_simAngleDifference;
+  private double m_simTurnAngleIncrement;
+
+  //private final SwerveDriveKinematics m_kinematics = new SwerveDriveKinematics(frontLeftLocation, frontRightLocation, backLeftLocation, backRightLocation);
+
   /** Creates a new SwerveModule. */
-  public SwerveModule(int driveMotorID, int turnMotorID, int drivingEncoderChannelA, int drivingEncoderChannelB,
-  int turningEncoderChannelA, int turningEncoderChannelB) {
-    driveMotor = new CANSparkMax(driveMotorID, MotorType.kBrushless);
-    turnMotor = new CANSparkMax(turnMotorID, MotorType.kBrushless);
-    //turnMotor 
+  public SwerveModule(
+      int driveMotorChannel,
+      int turningMotorChannel,
+      int turningEncoderChannel,
+      double turningEncoderOffsetDegrees) {
     
+    
+    m_driveMotor = new CANSparkMax(driveMotorChannel, MotorType.kBrushless);
+    m_turningMotor = new CANSparkMax(turningMotorChannel, MotorType.kBrushless);
+    //m_driveMotor.setInverted(true);
+    m_turningMotor.setInverted(true);
+
+    m_driveEncoder = m_driveMotor.getEncoder(SparkMaxRelativeEncoder.Type.kHallSensor, 42);
+    m_turningEncoder = m_turningMotor.getEncoder(SparkMaxRelativeEncoder.Type.kHallSensor, 42);
+
+    // Distance per pulse
+    m_driveEncoder.setPositionConversionFactor(2 * Math.PI * SwerveConstants.kWheelRadius / 6.75);
+    m_driveEncoder.setVelocityConversionFactor(2 * Math.PI * SwerveConstants.kWheelRadius / 6.75 / 60); // 60 seconds per minute
+
     driveSim = new FlywheelSim(DCMotor.getNeo550(1), 1, 1);
     turnSim = new FlywheelSim(DCMotor.getNeo550(1), 1, 1);
 
-    driveEncoder = new Encoder(drivingEncoderChannelA, drivingEncoderChannelB);
-    turnEncoder = new Encoder(turningEncoderChannelA, turningEncoderChannelB);
-    
-    driveEncoderSim = new EncoderSim(driveEncoder);
-    turnEncoderSim = new EncoderSim(turnEncoder);
+    // Radians per pulse
+     //m_turningEncoder.setDistancePerPulse(2 * Math.PI / SwerveConstants.kEncoderResolution);
+
+    // Limit the PID Controller's input range between -pi and pi and set the input
+    // to be continuous.
+    m_turningPIDController.enableContinuousInput(-Math.PI/2, Math.PI/2);
+
+    //m_turningEncoder.configMagnetOffset(-turningEncoderOffsetDegrees);
   }
 
-  public SwerveModuleState getState(){
-    return new SwerveModuleState(driveEncoder.getRate(), new Rotation2d(turnEncoder.getDistance()));
+  public SwerveModuleState getState() {
+    double m_moduleAngleRadians = m_angleEncoder.getAbsolutePosition() * 2 * Math.PI / 360;
+
+    return new SwerveModuleState(
+        m_driveEncoder.getVelocity(), new Rotation2d(m_moduleAngleRadians));
   }
 
   public SwerveModulePosition getPosition() {
-    return new SwerveModulePosition(driveEncoder.getDistance(), new Rotation2d(turnEncoder.getDistance()));
+    double m_moduleAngleRadians = m_angleEncoder.getAbsolutePosition() * 2 * Math.PI / 360;
+
+    return new SwerveModulePosition(
+        m_driveEncoder.getPosition(), new Rotation2d(m_moduleAngleRadians));
   }
 
+  public double getDriveMeters() {
+    if(RobotBase.isReal())
+      return m_driveEncoder.getPosition();
+    else
+        return m_simDriveEncoderPosition;
+  }
+  public double getDriveMetersPerSecond() {
+    if(RobotBase.isReal())
+      return m_driveEncoder.getVelocity();
+    else
+      return m_simDriveEncoderVelocity;
+  }
+
+  private void simUpdateDrivePosition(SwerveModuleState state) {
+    m_simDriveEncoderVelocity = state.speedMetersPerSecond;
+    double distancePer20Ms = m_simDriveEncoderVelocity / 50.0;
+
+    m_simDriveEncoderPosition += distancePer20Ms;
+  }
+  private void simTurnPosition(double angle) {
+    if (angle != m_currentAngle && m_simTurnAngleIncrement == 0) {
+      m_simAngleDifference = angle - m_currentAngle;
+      m_simTurnAngleIncrement = m_simAngleDifference / 20.0;// 10*20ms = .2 sec move time
+    }
+
+    if (m_simTurnAngleIncrement != 0) {
+      m_currentAngle += m_simTurnAngleIncrement;
+
+      if ((Math.abs(angle - m_currentAngle)) < .1) {
+        m_currentAngle = angle;
+        m_simTurnAngleIncrement = 0;
+      }
+    }
+
+    private void simUpdateDrivePosition(SwerveModuleState state) {
+      m_simDriveEncoderVelocity = state.speedMetersPerSecond;
+      double distancePer20Ms = m_simDriveEncoderVelocity / 50.0;
   
+      m_simDriveEncoderPosition += distancePer20Ms;
+    }
+    private void simTurnPosition(double angle) {
+      if (angle != m_currentAngle && m_simTurnAngleIncrement == 0) {
+        m_simAngleDifference = angle - m_currentAngle;
+        m_simTurnAngleIncrement = m_simAngleDifference / 20.0;// 10*20ms = .2 sec move time
+      }
+  
+      if (m_simTurnAngleIncrement != 0) {
+        m_currentAngle += m_simTurnAngleIncrement;
+  
+        if ((Math.abs(angle - m_currentAngle)) < .1) {
+          m_currentAngle = angle;
+          m_simTurnAngleIncrement = 0;
+        }
+      }
+    }
+  }
+
+  public void setDesiredState(SwerveModuleState desiredState) {
+    double m_moduleAngleRadians = m_angleEncoder.getAbsolutePosition() * 2 * Math.PI / 360;
 
 
-  public void setDesiredState(SwerveModuleState desiredState){
-    SwerveModuleState state = SwerveModuleState.optimize(desiredState, new Rotation2d(turnEncoder.getDistance()));
+    // Optimize the reference state to avoid spinning further than 90 degrees
+    SwerveModuleState state =
+        SwerveModuleState.optimize(desiredState, new Rotation2d(m_moduleAngleRadians));
+
+    // Calculate the drive output from the drive PID controller.
+    final double driveOutput =
+        m_drivePIDController.calculate(m_driveEncoder.getVelocity(), state.speedMetersPerSecond);
     
-    driveOutput = drivePIDController.calculate(driveEncoder.getRate(), state.speedMetersPerSecond);
+    
+    // final double driveFeedforward = m_driveFeedforward.calculate(state.speedMetersPerSecond);
 
-    double driveFeedForwardOutput = driveFeedforward.calculate(state.speedMetersPerSecond);
+    // Calculate the turning motor output from the turning PID controller.
+    final double turnOutput =
+        m_turningPIDController.calculate(m_moduleAngleRadians, state.angle.getRadians());
 
-    turnOutput = turnPIDController.calculate(turnEncoder.getRate(), state.angle.getRadians());
+    // final double turnFeedforward =
+    //     m_turnFeedforward.calculate(m_turningPIDController.getSetpoint().velocity);
 
-    double turnFeedForwardOutput = turnFeedforward.calculate(turnPIDController.getSetpoint());
-
-    driveMotor.set(driveOutput+driveFeedForwardOutput);
-    turnMotor.set(turnOutput+turnFeedForwardOutput);
+    m_turningMotor.set(turnOutput);
+    //System.out.println(turnOutput);
+    m_driveMotor.set(driveOutput);
   }
 
   public double getDriveCurrent() {
@@ -109,6 +219,19 @@ public class SwerveModule extends SubsystemBase {
   public double getTurnCurrent() {
     return turnSim.getCurrentDrawAmps();
   }
+
+  public double getHeadingDegrees() {
+    if(RobotBase.isReal())
+      return m_turningEncoder.getPosition();
+    else
+      return m_currentAngle;
+  }
+
+  public Rotation2d getHeadingRotation2d() {
+    return Rotation2d.fromDegrees(getHeadingDegrees());
+  }
+
+  
 
   /*public CANSparkMax getDriveMotor(){
     return driveMotor;
@@ -132,24 +255,23 @@ public class SwerveModule extends SubsystemBase {
   }
 
   public void simulationInit() {
-    REVPhysicsSim.getInstance().addSparkMax(driveMotor, DCMotor.getNeo550(1));
-    REVPhysicsSim.getInstance().addSparkMax(turnMotor, DCMotor.getNeo550(1));
+    REVPhysicsSim.getInstance().addSparkMax(m_driveMotor, DCMotor.getNeo550(1));
+    REVPhysicsSim.getInstance().addSparkMax(m_turningMotor, DCMotor.getNeo550(1));
   }
 
   public void simulationPeriodic() {
     simulationInit(); //set up roborio / enable simulator
     REVPhysicsSim.getInstance().run();
 
-    setDesiredState(getState()); //set next setpoint + optimize (feedforward)
-
-    driveSim.setInputVoltage(driveMotor.get()*RobotController.getBatteryVoltage());
-    turnSim.setInputVoltage(turnMotor.get()*RobotController.getBatteryVoltage());
+    driveSim.setInputVoltage(m_driveMotor.get()*RobotController.getBatteryVoltage());
+    turnSim.setInputVoltage(m_turningMotor.get()*RobotController.getBatteryVoltage());
 
     double drawCurrent = getDriveCurrent()+getTurnCurrent();
     double loadedVoltage = BatterySim.calculateDefaultBatteryLoadedVoltage(drawCurrent);
     RoboRioSim.setVInVoltage(loadedVoltage);
 
-    System.out.println("Working");
+    //setDesiredState(getState()); //set next setpoint w/ position
+    //System.out.println("Working");
 
     //REVPhysicsSim.getInstance().run();
     //driveSim.setInputVoltage(getDriveSpeed());
