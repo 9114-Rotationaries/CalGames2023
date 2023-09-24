cf75b59eb9ffe7f1b356f9683c04ea319139d095
Zhou-Anthony <antzho803@gmail.com>
2023-09-24 00:04:48 -0700

    getDistance & checkDistance for limelight

---
+++ src/main/java/frc/robot/subsystems/CheckDistance.java
@@ -0,0 +1,35 @@
+package frc.robot.subsystems;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class CheckDistance extends CommandBase {
+    private Vision vision;
+ 
+    public CheckDistance(Vision vision) { // Pass the Vision instance to the constructor
+        this.vision = vision;
+        // You can initialize other variables or do additional setup here if needed.
+        // limelightTable = NetworkTableInstance.getDefault().getTable("limelight");
+    }
+
+    public void checkDistance() {
+        // Check distance to charging station using Limelight
+        // Make sure you have access to the 'limelightTable' instance here.
+        double distance = Vision.limelightTable.getEntry("distance").getDouble(0.0); // Replace "distance" with the actual Limelight key
+
+        double targetDistance = 10.0; // Adjust this value as needed
+        double error = targetDistance - distance;
+
+        if (error > 0.1) {
+            // Notify that it is too far.
+            SmartDashboard.putNumber("Too far by: ", error);
+        } else if (error < -0.1) {
+            // Notify that it is too close
+            SmartDashboard.putNumber("Too close by: ", error);
+        } else {
+            // Notify that ready for forklift down.
+            SmartDashboard.putNumber("Ready to dock | Error is: ", error);
+        }
+    }
+}
