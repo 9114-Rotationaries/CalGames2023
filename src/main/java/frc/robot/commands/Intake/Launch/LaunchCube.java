eb84193aef96a6fcce646aee524cc94a9b8b91f9
Rotationaries <rotationaries@gmail.com>
2023-10-07 09:40:04 -0700

    timer

--- src/main/java/frc/robot/commands/Intake/Launch/LaunchCube.java
+++ src/main/java/frc/robot/commands/Intake/Launch/LaunchCube.java
@@ -4,12 +4,17 @@
 
 package frc.robot.commands.Intake.Launch;
 
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants.IntakeConstants;
 import frc.robot.subsystems.Intake;
 
 public class LaunchCube extends CommandBase {
   private Intake intake;
+  private boolean finishedFull;
+  private boolean finishedIntake;
+  private Timer timer = new Timer();
   /** Creates a new LaunchObject. */
   public LaunchCube(Intake intake) {
     this.intake=intake;
@@ -19,23 +24,38 @@ public class LaunchCube extends CommandBase {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    finishedFull = false;
+    finishedIntake = false;
+    timer.start();
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    intake.cubeLaunch(IntakeConstants.cubeLaunchSpeed);
+    SmartDashboard.putNumber("timer", timer.get());
+    intake.cubeIntake(IntakeConstants.cubeIntakeSpeed);
+    if (timer.get() > 5) {
+      finishedIntake = true;
+      intake.cubeOuttake(IntakeConstants.cubeOuttakeSpeed);
+    }
+    if (timer.get() < 10) {
+      intake.stop();
+      finishedFull = true;
+    }
+
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
     intake.stop();
+    timer.reset();
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return finishedFull;
   }
 }
